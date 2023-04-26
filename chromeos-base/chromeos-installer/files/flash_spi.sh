7e5371b64a617719e08fa72405a1ae5fe402b7b1
Su Yue <glass@fydeos.io>
2023-04-26 17:57:48 +0800

    chromeos-installer: try to not flash mtd again
    Signed-off-by: Su Yue <glass@fydeos.io>

--- chromeos-base/chromeos-installer/files/flash_spi.sh
+++ chromeos-base/chromeos-installer/files/flash_spi.sh
@@ -8,8 +8,10 @@ main() {
  local zero="${LOADER_DIR}/zero.img"
  local loader="${LOADER_DIR}/spi.img"
 
- cmp "$loader" "$target"
- if [ "$?" -ne 0 ]; then
+ local target_md5="$(md5sum ${target} | awk '{print $1}')"
+ local loader_md5="$(md5sum ${loader} | awk '{print $1}')"
+
+ if [ "$target_md5" != "$loader_md5" ]; then
      echo "installing $loader to $target"
  #    dd if=$zero of=$target status=progress bs=1024 conv=fdatasync,notrunc
      dd if=$loader of=$target status=progress bs=1024 conv=fdatasync,notrunc

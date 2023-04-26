5efa8b13ce4600bd26d61d83ca77ab98a1ee7bd7
Su Yue <glass@fydeos.io>
2023-04-26 15:14:43 +0800

    chromeos-base/chromeos-installer: avoid unnecessary mtd flash
    Signed-off-by: Su Yue <glass@fydeos.io>

--- chromeos-base/chromeos-installer/files/flash_spi.sh
+++ chromeos-base/chromeos-installer/files/flash_spi.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 LOADER_DIR=/usr/share/rock5b
 
@@ -8,10 +8,13 @@ main() {
  local zero="${LOADER_DIR}/zero.img"
  local loader="${LOADER_DIR}/spi.img"
 
- echo "installing $loader to $target"
- dd if=$zero of=$target status=progress bs=1024 conv=fdatasync,notrunc
- dd if=$loader of=$target status=progress bs=1024 conv=fdatasync,notrunc
- echo "done"
+ cmp "$loader" "$target"
+ if [ "$?" -ne 0 ]; then
+     echo "installing $loader to $target"
+ #    dd if=$zero of=$target status=progress bs=1024 conv=fdatasync,notrunc
+     dd if=$loader of=$target status=progress bs=1024 conv=fdatasync,notrunc
+     echo "done"
+ fi
 }
 
 main "$@"
